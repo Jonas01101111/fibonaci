13286578ca6971f33ce6d7e46d9b4b26b3cd5a5c
Jonas01101111 <jonas1kiubi@gmail.com>
2024-09-30 14:48:48 -0300

    Adicionar arquivos de projeto.

---
+++ fibonaci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonaci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 0, 1, 1};
+            Console.WriteLine(numeros[0]+", ");
+            while (true) {
+                
+                numeros[2] = numeros[0]+numeros[1];
+                
+                numeros[0]=numeros[1];
+                numeros[1]=numeros[2];
+                Console.Write(numeros[0]+", ");
+                
+            }
+
+        }
+    }
+}
